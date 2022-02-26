2853ce32337a547d8988ed6a0a407beb133084d5
Itukachan <yokotaituka@gmail.com>
2022-02-26 15:14:28 +0900

    first commit

---
+++ resources/views/menu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>メインメニュー</title>
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<h3>INFO</h3>
+	<br>
+	<a href="/test">第36回国家試験情報</a>
+	<br>
+	<br>
+	<a href="/recruit">アルバイト・インターン求人情報</a>
+	<br>
+	<br>
+	<h3>MENU</h3>
+	<a href="/question">演習問題</a>
+	<br>
+	<br>
+	<a href="/teaching">栄養指導</a>
+	<br>
+	<br>
+	<a href="/recipe">レシピ共有</a>
+	<br>
+	<br>
+	<a href="/member">会員情報詳細</a>
+	<br>
+	<br>
+	<h3>ABOUT</h3>
+	<a href="/question">サイト情報</a>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<a href="/login">ログアウト</a>
+</body>
+</html>
---
+++ resources/views/top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <!-- http://localhost/first_laravel/public/ -->
+<head>
+<meta charset="UTF-8">
+<title>Top</title>
+</head>
+<body>
+<form action="menu">
+<input type="submit" value="start">
+</form>
+</body>
+</html>
